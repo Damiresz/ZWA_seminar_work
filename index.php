00ec4fcf519aba77c031de45a18cfe954b277c8e
Damir <damirabd879@gmail.com>
2023-11-15 15:35:56 +0000

    commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE php>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  dnes je <?php
+  echo date("d.m.Y");
+  echo ", ".date("h:i").".";
+  ?>
+</body>
+</html>
